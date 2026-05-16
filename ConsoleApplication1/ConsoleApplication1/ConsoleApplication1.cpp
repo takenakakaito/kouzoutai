42977528f5023dfe1bb45b03ac1efa0335ce46cd
takenakakaito <ktc25a32a0010@edu.kyoto-tech.ac.jp>
2026-05-16 16:16:44 +0900

    5/16
    課題１，課題2の作成完了

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿// ConsoleApplication1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <stdio.h>
+struct Parameter
+{
+    char name[10];
+    int hp;
+    int attack;
+    int defense;
+};
+void DispParameter(Parameter param)
+{
+    printf("Name:%s\n", param.name);
+    printf("HP:%d\n", param.hp);
+    printf("Attack:%d\n", param.attack);
+    printf("Defense:%d\n", param.defense);
+}
+int main()
+{
+    Parameter A = { "Rui",10,99,1 };
+
+    Parameter F = { "Riku",32,24,69 };
+    DispParameter(A);
+    DispParameter(F);
+    return 0;
+}
+
---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,51 @@
+﻿// ConsoleApplication2.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <stdio.h>
+struct Score
+{
+    char name[10];
+    int score;
+};
+void DispScore(Score score)
+{
+    printf("Name:%s\n", score.name);
+    printf("Score:%d\n", score.score);
+}
+int main()
+{
+    int total=0;
+    float ave=0;
+    int max=0;
+    Score A = { "Raiki",44 };
+    Score B = { "Raito",34 };
+    Score C = { "Yusei",16 };
+    Score D = { "Mahiro",32 };
+    Score E = { "Naoto",9 };
+    DispScore(A);
+    DispScore(B);
+    DispScore(C);
+    DispScore(D);
+    DispScore(E);
+    Score students[5] = { A,B,C,D,E };
+    for (int i = 0; i < 5; i++)
+    {
+        total += students[i].score;
+        if (max<students[i].score)
+        {
+            max = students[i].score;
+        }
+    }
+    ave = (float)total / 5;
+    printf("平均:%f",ave);
+    for (int j = 0; j < 5; j++)
+    {
+        if (max == students[j].score)
+        {
+            printf("最高得点:%d", students[j].score);
+            printf("名前:%s", students[j].name);
+        }
+    }
+    
+}
+
